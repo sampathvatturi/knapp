19779c65a8dd8aeb702dbe8fd708c87ea959bee7
roop-sagarh <talariroopsagar@gmail.com>
2022-11-25 15:22:31 +0530

    Revert "Revert "adding new modules""
    This reverts commit aaa2734c2075135dc96edf4ecc3adf7b67a67681.

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -3,10 +3,20 @@ import {
 } from '@angular/core';
 import { ModulesRoutingModule } from './modules-routing.module';
 import { DEPARTMENT_COMPONENTS } from './department';
+import { TenderDetailsComponent } from './tender-details/tender-details.component';
+import { InvoicesComponent } from './invoices/invoices.component';
+import { ApprovalsComponent } from './approvals/approvals.component';
+import { PaymentProceedingsComponent } from './payment-proceedings/payment-proceedings.component';
+import { InventoryItemsComponent } from './inventory-items/inventory-items.component';
+import { FundsComponent } from './funds/funds.component';
+import { ExpenditureComponent } from './expenditure/expenditure.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ForgotPasswordComponent } from './user/forgot-password/forgot-password.component';
 
 @NgModule({
     declarations: [        
-        ...DEPARTMENT_COMPONENTS,
+        ...DEPARTMENT_COMPONENTS, TenderDetailsComponent, InvoicesComponent, ApprovalsComponent, PaymentProceedingsComponent, InventoryItemsComponent, FundsComponent, ExpenditureComponent, LoginComponent, RegisterComponent, ForgotPasswordComponent,
     ],
     imports: [ModulesRoutingModule],
 })
