410435694ec95a760b54125191e87417bc225804
Roop Sagarh Talari <talariroopsagar@gmail.com>
2022-11-26 13:53:31 +0530

    department list component
    added services for table data, added side drawer functionality

--- src/app/modules/department/components/department-list/department-list.component.ts
+++ src/app/modules/department/components/department-list/department-list.component.ts
@@ -1,59 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-interface Department {
-  department_id: number,
-      department_name: string,
-      ranking: number,
-      status: string,
-      created_date:string,
-      created_by:number,
-      updated_date:string,
-      updated_by: number ,
-      department_code:string
-};
+import { FormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
+import { DepartmentService } from 'src/app/services/department.service';
+
 @Component({
   selector: 'app-department-list',
   templateUrl: './department-list.component.html',
   styleUrls: ['./department-list.component.css']
 })
 export class DepartmentListComponent implements OnInit {
-
-  constructor() { }
+  listOfData: any[] =[];
+  visible = false;
+  drawerTitle:string='';
+  departmentform!:FormGroup
+  constructor(private departmentservice: DepartmentService, private fb:UntypedFormBuilder) { }
 
   ngOnInit(): void {
+    this.departmentform = this.fb.group({
+      department_id: [null, [Validators.required]],
+      department_name: [null, [Validators.required]],
+      ranking: [null, [Validators.required]],
+      status: [null, [Validators.required]],
+      created_date: [null, [Validators.required]],
+      created_by: [null, [Validators.required]],
+      updated_date: [null, [Validators.required]],
+      updated_by: [null, [Validators.required]],
+      department_code: [null, [Validators.required]],
+    });
+      this.departmentservice.getData()
+      .subscribe(list => {
+        this.listOfData = list;
+      });
   }
-  visible = false;
-
-  open(): void {
+  edit(data:any){
+    this.drawerTitle = "Edit";
     this.visible = true;
+    this.departmentform = this.fb.group({
+      department_id: [data.department_id, [Validators.required]],
+      department_name: [data.department_name, [Validators.required]],
+      ranking: [data.ranking, [Validators.required]],
+      status: [data.status, [Validators.required]],
+      created_date: [data.created_date, [Validators.required]],
+      created_by: [data.created_by, [Validators.required]],
+      updated_date: [data.updated_date, [Validators.required]],
+      updated_by: [data.updated_by, [Validators.required]],
+      department_code: [data.department_code, [Validators.required]],
+    });
+  }
+  open(): void {
+    this.drawerTitle = "New";
+    this.visible = true;
+    this.departmentform = this.fb.group({
+      department_id: [null, [Validators.required]],
+      department_name: [null, [Validators.required]],
+      ranking: [null, [Validators.required]],
+      status: [null, [Validators.required]],
+      created_date: [null, [Validators.required]],
+      created_by: [null, [Validators.required]],
+      updated_date: [null, [Validators.required]],
+      updated_by: [null, [Validators.required]],
+      department_code: [null, [Validators.required]],
+    });
   }
 
   close(): void {
     this.visible = false;
   }
-  listOfData: Department[] = [
-    {
-      department_id: 1,
-      department_name: 'dept1',
-      ranking: 2,
-      status: 'active',
-      created_date:'2022-11-24 11:46:48',
-      created_by:0,
-      updated_date:'2022-11-24 11:46:50',
-      updated_by: 0 ,
-      department_code:''
-    },
 
-    {
-      department_id: 2,
-      department_name: 'dept2',
-      ranking: 1,
-      status: 'active',
-      created_date:'2022-11-24 11:46:48',
-      created_by:0,
-      updated_date:'2022-11-24 11:46:50',
-      updated_by: 0 ,
-      department_code:''
-    },
-
-  ];
 }
