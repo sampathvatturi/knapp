b1aa3f14bf3db7a497000cda2321d58f9f6bdfae
Sampath <sampathvatturi@gmail.com>
2022-11-30 10:50:40 +0530

    Update environment.ts

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000'
+  // apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://ec2-34-211-130-130.us-west-2.compute.amazonaws.com:8080'
 };
 
 /*
