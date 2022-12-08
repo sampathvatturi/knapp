4c8a08eacef24d8e669a6749c90d27a357375b23
MrutyunjayaRao <mjr.mca@gmail.com>
2022-12-08 17:03:12 +0530

    Validation Pattern issues fixed
    Validation Pattern issues fixed

--- Frontend/src/app/shared/global_constants.ts
+++ Frontend/src/app/shared/global_constants.ts
@@ -8,14 +8,14 @@ export class GlobalConstants {
 
 
     //regex
-    public static nameRegex: string = "^[[]^_][a-zA-z0-9 ]*";
+    public static nameRegex: string = "[a-zA-z0-9 ]*";
     public static emailRegex: string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
     public static contactNumberRegex: string = "^[e0-9]{10,10}$";
     public static numberRegex: string  = "[0-9]*"
     public static addressRegex: string = "[-a-zA-z0-9 /:,.\n]*";
-    public static firstLastNameRegex: string = "^[[]^][a-zA-z]*";    
+    public static firstLastNameRegex: string = "[a-zA-z]*";    
     public static amountRegex = /^\d{0,10}(\.?\d{0,2})$/;
-    
+
     public static error: string = "error";
 
     //department
