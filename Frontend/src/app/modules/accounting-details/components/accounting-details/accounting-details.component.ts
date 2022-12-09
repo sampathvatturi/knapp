1081628ce89e46517e55763a4d257acc0d6f30c3
saikumar3570 <saikumar3570@gmail.com>
2022-12-09 11:18:00 +0530

    accounting details view added, changes in vendors works,inventory

--- Frontend/src/app/modules/accounting-details/components/accounting-details/accounting-details.component.ts
+++ Frontend/src/app/modules/accounting-details/components/accounting-details/accounting-details.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-accounting-details',
   templateUrl: './accounting-details.component.html',
-  styleUrls: ['./accounting-details.component.css']
+  styleUrls: ['./accounting-details.component.css'],
 })
 export class AccountingDetailsComponent implements OnInit {
+  searchText = '';
+  accountDetails: any = [];
 
-  constructor() { }
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
+    // this.api.getCall('//').subscribe((res) =>{
+    //   this.accountDetails = res;
+    // })
   }
-
 }
