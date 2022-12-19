bc6500d6ae51c5fb4b8b31b44eb1b764a028bf5e
Roop Sagarh Talari <talariroopsagar@gmail.com>
2022-12-19 11:28:33 +0530

    survey report component created

--- Frontend/src/app/modules/survey-report/components/survey-report/survey-report.component.ts
+++ Frontend/src/app/modules/survey-report/components/survey-report/survey-report.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
+import { NotificationService } from 'src/app/services/auth/notification.service';
 
 @Component({
   selector: 'app-survey-report',
@@ -6,10 +8,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./survey-report.component.css']
 })
 export class SurveyReportComponent implements OnInit {
+  works_info: any = [];
+  searchText = '';
+  drawerTitle: string = '';
+  surveyForm!: FormGroup;
+  visible = false;
+  submit = true;
+  updateBtnDisable: boolean = false;
 
-  constructor() { }
+  constructor(private fb: UntypedFormBuilder,
+    private notification: NotificationService,) { }
 
   ngOnInit(): void {
   }
+  create(): void {
+    this.submit = true;
+    this.drawerTitle = 'Add Survey Report';
+    this.visible = true;
+    this.surveyFormValidators();
+  }
+  edit(type: any, data: any) {
 
+    this.submit = false;
+    this.drawerTitle = 'Edit Survey Report';
+    this.visible = true;
+    this.surveyFormValidators();
+    if (type === 'view') {
+      this.updateBtnDisable = false;
+    }
+  }
+  close(): void {
+    this.visible = false;
+  }
+  onCreateSubmit() {
+    if (this.surveyForm.valid) {
+
+    } else {
+      Object.values(this.surveyForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  onUpdateSubmit() {
+    if (this.surveyForm.valid) {
+      //service
+    } else {
+      Object.values(this.surveyForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  surveyFormValidators() {
+    this.surveyForm = this.fb.group({
+      status: ['', [Validators.required]],
+    });
+  }
 }
